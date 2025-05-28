315bcdb49f13cfdfe440a1564f35a17d53c7bc90
Luca Iantosco <84413484+Lukeino@users.noreply.github.com>
2025-05-28 11:12:40 +0200

    Add files via upload

---
+++ src/NuovaPagina.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NuovaPagina.css';
+
+function NuovaPagina() {
+  return (
+    <div className="nuova-pagina-container">
+      <h1 className="nuova-pagina-title">Benvenuto nella nuova pagina!</h1>
+      <p className="nuova-pagina-text">Questa è la pagina che si apre dopo aver cliccato su REGISTRATI.</p>
+    </div>
+  );
+}
+
+export default NuovaPagina;
